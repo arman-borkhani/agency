8719724e2ad7034dafb55c2e2b3e8ef7bc96c61a
arman-borkhani <armanborkhani@gmail.com>
2023-08-02 18:59:19 +0330

    Add button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,68 @@
+<template>
+  <a class="btn" v-if="link">
+    <slot></slot>
+  </a>
+  <button class="btn" v-else>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.btn {
+  padding: 0.75em 1.25em;
+  font-size: 1rem;
+  font-weight: 500;
+  border-width: 1px;
+  border-style: solid;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  text-decoration: none;
+  text-align: center;
+  display: inline-block;
+  transition: background-color 0.3s ease;
+}
+
+.btn--primary {
+  background-color: var(--secondary);
+  border-color: var(--secondary);
+  color: var(--white);
+}
+
+.btn--primary:hover {
+  background-color: rgba(66, 186, 150, 0.9);
+}
+
+.btn--outline-white {
+  border-color: var(--white);
+  color: var(--white);
+  background-color: transparent;
+}
+
+.btn--outline-white:hover {
+  background-color: rgba(255, 255, 255, 0.1);
+}
+
+.btn--small {
+  font-size: 0.875rem;
+}
+
+.btn--medium {
+  font-size: 1.125rem;
+}
+
+.btn--large {
+  font-size: 1.25rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import BaseContainer from './components/BaseContainer.vue';
+import BaseButton from './components/BaseButton';
 
 const app = createApp(App);
 app.component('base-container', BaseContainer);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
